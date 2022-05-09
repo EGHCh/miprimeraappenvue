832512dddc34746c670a068121461346a2942a06
EGHCh <63925946+EGHCh@users.noreply.github.com>
2022-05-09 03:44:41 -0400

    Desafio Completo

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Apellido</th>
+        <th scope="col">RUN</th>
+        <th scope="col">Nacimiento</th>
+        <th scope="col">edad</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(usuario, $index) of usuarios" :key="$index">
+        <td>{{ $index }}</td>
+        <td>{{ usuario.nombre }}</td>
+        <td>{{ usuario.apellido }}</td>
+        <td>{{ usuario.run }}</td>
+        <td>{{ usuario.nacimiento }}</td>
+        <td>{{ usuario.edad }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () =>({
+    usuarios : [
+      {
+        nombre: "jose",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      {
+        nombre: "raul",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      {
+        nombre: "pedro",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      {
+        nombre: "maria",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      {
+        nombre: "antonio",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      {
+        nombre: "pepe",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      {
+        nombre: "karen",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      {
+        nombre: "constanza",
+        apellido: "perez",
+        run: "123456k",
+        nacimiento: "12-04-67",
+        edad: "12",
+      },
+      ]
+    
+  }),
 }
 </script>
 
